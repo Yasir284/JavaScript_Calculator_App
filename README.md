6dfad2a799588d1a292285716a9a52e5ac148800
Yasir284 <lambawalay@gmail.com>
2022-09-27 19:32:46 +0530

    change in readme

---
+++ README.md
@@ -0,0 +1,45 @@
+![badge](https://img.shields.io/badge/MADE%20WITH-HTML,%20CSS%20&%20JS-blue)
+![badge](https://img.shields.io/badge/TIME%20TAKEN-10%20to%2012%20hrs-red)
+[![badge](https://img.shields.io/badge/SEE%20DEMO%20-VISIT-green)](https://javascript-calculatorapp-project1.netlify.app/)
+
+# JavaScript Calculator App
+
+- This is Javascript Project. In this project I have made a **Calculator App** with JavasCript, which performes addition, multiplicaption, division and subtraction by BODMAS rule.
+
+- You can also use keybord for calculation.
+
+- It took me 10-12 hrs to make this project.
+
+  [![badge](https://img.shields.io/badge/LINK%20OF-PROJECT-blue)](https://javascript-calculatorapp-project1.netlify.app/)
+
+---
+
+## 🛠 Skills learned
+
+- JavaScript
+- To Use DOM
+- Use of keypress event of addEventListner
+- Use of onchange attribute
+- Creating Elements in Javascript
+- Adding Elements in HTML using JavaScript
+- Adding Attributes to the Elements
+
+---
+
+## 🎥 Working
+
+![Gif](./js_project7.gif)
+
+---
+
+## 🚀 About Me
+
+### Hey, I am Yasir lambawala
+
+- I am born and brought up in Vadodara, Gujarat. I have done my Btech at GTU university and I am currently learning Web-development.
+
+---
+
+## Feedback
+
+- If you have any feedback, please reach me at lambawalay@gmail.com
--- script.js
+++ script.js
@@ -6,15 +6,25 @@ let textView = document.querySelector(".textView");
 let btn = document.getElementsByClassName("btn");
 let clearBtn = document.querySelector(".red");
 
-function insert(value) {
-  if (isNaN(value) && isNaN(textView.value[textView.value.length - 2])) {
-    textView.value = textView.value.slice(0, textView.value.length - 1);
+function insert(val) {
+  if (
+    val != "." &&
+    isNaN(val) &&
+    textView.value.length > 3 &&
+    isNaN(textView.value[textView.value.length - 2])
+  ) {
+    textView.value = textView.value.slice(0, textView.value.length - 3);
   }
-  textView.value += value;
+  textView.value += val;
 }
 
 function back() {
   let text = textView.value.split("");
+  console.log(text);
+  if (textView.value[textView.value.length - 1] === " ") {
+    text.pop();
+    text.pop();
+  }
   text.pop();
   textView.value = text.join("");
 }
@@ -49,34 +59,108 @@ function getOperatorIndex(text, operator) {
 }
 
 function equals() {
-  let numbers = textView.value.split(" ").filter((item) => !isNaN(item));
-  let operators = textView.value.split(" ").filter((item) => isNaN(item));
-  let allOperators = ["/", "*", "+", "-"];
+  let text = textView.value.split(" ");
 
-  for (ourOperator of allOperators) {
-    while (operators.includes(ourOperator)) {
-      const index = operators.indexOf(ourOperator);
-      const answer = operation[ourOperator](
-        Number(numbers[index]),
-        Number(numbers[index + 1])
-      );
-      numbers.splice(index, 2, answer);
-      operators.splice(index, 1);
+  let operators = ["/", "*", "+", "-"];
+  for (operator of operators) {
+    if (text.includes(operator)) {
+      let index = getOperatorIndex(text, operator);
+      while (index.length > 0) {
+        let leftIndex = index[0] - 1;
+        let rightIndex = index[0] + 1;
+        let left = parseInt(text[leftIndex]);
+        let right = parseInt(text[rightIndex]);
+        let evaluate = operation[operator](left, right);
+        text.splice(leftIndex, 3, evaluate);
+        index = getOperatorIndex(text, operator);
+      }
     }
   }
-  console.log(numbers.join(""));
-  return (textView.value = numbers.join(""));
+  console.log(text.join(""));
+  return (textView.value = text.join(""));
 }
-// 22 + 5 + 12 / 2 / 2 + 1
-// if (text.includes(operator)) {
-//   let index = getOperatorIndex(text, operator);
-//   while (index.length > 0) {
-//     let leftIndex = index[0] - 1;
-//     let rightIndex = index[0] + 1;
-//     let left = parseInt(text[leftIndex]);
-//     let right = parseInt(text[rightIndex]);
-//     let evaluate = operation[operator](left, right); //4
-//     text.splice(leftIndex, 3, evaluate);
-//     index = getOperatorIndex(text, operator);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "/":
+      insert(" / ");
+      break;
+    case "*":
+      insert(" * ");
+      break;
+    case "+":
+      insert(" + ");
+      break;
+    case "-":
+      insert(" - ");
+      break;
+    case ".":
+      insert(e.key);
+      break;
+    case "Enter":
+      equals();
+      break;
+    case "Backspace":
+      back();
+      break;
+    case "Delete":
+      clearCalc();
+      break;
+    case "0":
+      insert(e.key);
+      break;
+    case "1":
+      insert(e.key);
+      break;
+    case "2":
+      insert(e.key);
+      break;
+    case "3":
+      insert(e.key);
+      break;
+    case "4":
+      insert(e.key);
+      break;
+    case "5":
+      insert(e.key);
+      break;
+    case "6":
+      insert(e.key);
+      break;
+    case "7":
+      insert(e.key);
+      break;
+    case "8":
+      insert(e.key);
+      break;
+    case "9":
+      insert(e.key);
+      break;
+
+    default:
+      break;
+  }
+
+  console.log(e.key);
+});
+
+// ======Optimized solution =====
+// function equals() {
+//   let numbers = textView.value.split(" ").filter((item) => !isNaN(item));
+//   let operators = textView.value.split(" ").filter((item) => isNaN(item));
+//   let allOperators = ["/", "*", "+", "-"];
+
+//   for (ourOperator of allOperators) {
+//     while (operators.includes(ourOperator)) {
+//       const index = operators.indexOf(ourOperator);
+//       const answer = operation[ourOperator](
+//         Number(numbers[index]),
+//         Number(numbers[index + 1])
+//       );
+//       numbers.splice(index, 2, answer);
+//       operators.splice(index, 1);
+//     }
 //   }
+//   console.log(numbers.join(""));
+//   return (textView.value = numbers.join(""));
 // }
